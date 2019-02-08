54c04f685040b0bff9341835a762db581a50328f
hospitalization <keshasha@gmail.com>
2019-02-08 12:56:47 +0900

    code to go to sub activity deliver intent get intent change text on textView

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val val0:String = "This is val0"
+        val val1:String = "This is val1"
+
+        buttonSub0.setOnClickListener{
+            startActivity<Sub0Activity>(
+                "val0" to val0,
+                "val1" to val1
+            )
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Sub0Activity.kt
+++ app/src/main/java/com/example/myapplication/Sub0Activity.kt
@@ -2,11 +2,18 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub0.*
 
 class Sub0Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub0)
+
+        val val0 = intent.getStringExtra("val0").toString()
+        val val1 = intent.getStringExtra("val1").toString()
+
+        textViewSub0.text = val0
+        textViewSub1.text = val1
     }
 }
